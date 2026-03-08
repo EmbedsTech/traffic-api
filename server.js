c5906b1fa1c2fa00e2a32cdae96fa21072b60dfa
EmbedsTech <sanobar8620@gmail.com>
2026-03-08 19:53:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const admin = require("firebase-admin");
-
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
@@ -65,6 +63,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
   console.log("Server running on port " + PORT);
 });
+
 
 
 
